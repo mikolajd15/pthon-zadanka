8b4d37f82ac0e552e23e320f9c768e816c4df5a8
Nikolaij <mikolajd4agh@gmail.com>
2018-06-03 13:42:42 +0200

    files

--- mdig-agh-air_inf1_lab_py_exercises-271bc1def13b/skeletons/files-1/files.py
+++ mdig-agh-air_inf1_lab_py_exercises-271bc1def13b/skeletons/files-1/files.py
@@ -1,2 +1,14 @@
+import os
+
+
 def write_to_file(path: str) -> bool:
-    raise NotImplementedError
+    if os.path.isfile(path):
+        with open(path, 'w') as file:
+            file.write("Kilroy was here!")
+        return True
+    else:
+        return False
+
+
+
+
